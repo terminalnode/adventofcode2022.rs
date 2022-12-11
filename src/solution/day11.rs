0ef64595e1ecd66ad2bb7eff37ecdd393c3c6ce2
Alexander Rundberg <alexanderrundberg@fastmail.jp>
2022-12-11 19:28:37 +0100

    add solution day 11 part 1

--- src/solution/day11.rs
+++ src/solution/day11.rs
@@ -1,4 +1,4 @@
-use std::str::Lines;
+use std::fmt::{Debug};
 use crate::solution::day11::Operation::{Add, Multiply, Square};
 use crate::solution::day11::Test::Divisible;
 use crate::solution::Solution;
@@ -19,14 +19,43 @@ enum Test {
 
 #[derive(Debug)]
 struct Monkey {
-    items: Vec<i32>,
+    items: Vec<i64>,
     operation: Operation,
     test: Test,
     on_true: usize,
     on_false: usize,
+    inspects: usize,
 }
 
 impl Monkey {
+    fn do_round(&mut self) -> Vec<(usize, i64)> {
+        let tosses = self.items.iter().map(|item| {
+            // Inspect
+            //  - Run operation to modify worry level
+            //  - Divide by 3 (rounded down to nearest integer)
+            self.inspects += 1;
+            let worry = match self.operation {
+                Square => item * item,
+                Multiply(n) => item * (n as i64),
+                Add(n) => item + (n as i64)
+            } / 3;
+
+            // Run test and decide where to throw
+            let test = match self.test {
+                Divisible(n) => worry % (n as i64) == 0,
+            };
+            let target = if test { self.on_true } else { self.on_false };
+            (target, worry)
+        }).collect::<Vec<(usize, i64)>>();
+
+        self.items.clear();
+        tosses
+    }
+
+    fn hand_item(&mut self, worry: i64) {
+        self.items.push(worry);
+    }
+
     // region parse helpers
     fn new(input: &str) -> Result<Self, String> {
         let mut lines = input.lines().into_iter();
@@ -38,15 +67,16 @@ impl Monkey {
             test: Monkey::parse_test(lines.next().ok_or_else(|| "Failed to read test")?)?,
             on_true: Monkey::parse_if_true(lines.next().ok_or_else(|| "Failed to read if true")?)?,
             on_false: Monkey::parse_if_false(lines.next().ok_or_else(|| "Failed to read if false")?)?,
+            inspects: 0,
         };
         Ok(monkey)
     }
 
-    fn parse_items(input: &str) -> Result<Vec<i32>, String> {
+    fn parse_items(input: &str) -> Result<Vec<i64>, String> {
         input[18..].split(", ")
             .map(|it| {
-                it.parse::<i32>().or_else(|e| Err(format!("Failed to parse item {it}: {e}")))
-            }).collect::<Result<Vec<i32>, String>>()
+                it.parse::<i64>().or_else(|e| Err(format!("Failed to parse item {it}: {e}")))
+            }).collect::<Result<Vec<i64>, String>>()
     }
 
     fn parse_operation(input: &str) -> Result<Operation, String> {
@@ -106,7 +136,28 @@ impl Solution for Day11 {
 
     fn part1(&self) -> Result<String, String> {
         let mut monkeys = self.parse()?;
+
         monkeys.iter().for_each(|x| println!("{:?}", x));
-        todo!()
+        for round in 0..20 {
+            println!("Round {round}");
+            for i in 0..monkeys.len() {
+                let monkey = &mut monkeys[i];
+                let tosses = monkey.do_round();
+
+                for (ti, worry) in tosses {
+                    let target = &mut monkeys[ti];
+                    target.hand_item(worry);
+                }
+            }
+            monkeys.iter().for_each(|x| println!("{:?}", x));
+            println!();
+        }
+
+        let mut inspects = monkeys.iter()
+            .map(|m| m.inspects as i64)
+            .collect::<Vec<i64>>();
+        inspects.sort();
+        let product: i64 = inspects.iter().rev().take(2).product();
+        Ok(format!("Monkey business: {product}"))
     }
 }
